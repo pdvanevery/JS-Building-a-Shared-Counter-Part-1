87f29003d29798f83e47ca4682f3102956e945fd
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-02-02 19:24:28 -0800

    added fetch request but failed

--- public/index.js
+++ public/index.js
@@ -1,9 +1,16 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+   
+    const response = await fetch('http://http://127.0.0.1/:9001/counter');
+    console.log('response', data)
+       
+    
+    const result = await response.json();
+        
 
-    let countValue = 0;
+    let countValue = result.value;
 
     function increment(){
         countValue++;
